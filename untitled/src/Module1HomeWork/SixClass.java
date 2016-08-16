c84050c5f151ee56efb986522afb81abfaa4e079
usonictw <usonictw@bigmir.net>
2016-08-16 23:28:59 +0300

    Java Core Course

---
+++ untitled/src/Module1HomeWork/SixClass.java
@@ -0,0 +1,11 @@
+package Module1HomeWork;
+
+/**
+ * Created by PC on ${DATA}.
+ */
+public class SixClass {
+
+    public static void main(String[] args) {
+
+    }
+}
--- untitled/src/Module1HomeWork/ThirdClass.java
+++ untitled/src/Module1HomeWork/ThirdClass.java
@@ -5,6 +5,7 @@ package Module1HomeWork;
  */
 public class ThirdClass {
     public static void main(String[] args) {
+
         System.out.println("My third Class in Java");
     }
 }
