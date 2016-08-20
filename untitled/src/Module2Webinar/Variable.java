f1aba5526f3595e30b7ca866d49bc4146cc761e1
usonictw <usonictw@bigmir.net>
2016-08-20 11:01:12 +0300

    Java Core Course

---
+++ untitled/src/Module2Webinar/Variable.java
@@ -0,0 +1,46 @@
+package Module2Webinar;
+
+/**
+ * Created by PC on ${DATA}.
+ */
+public class Variable {
+
+    public static void main(String[] args) {
+
+
+        // number variable
+        Integer a1;
+        int a, a2;
+        double b = 3.14;
+        float c;
+        byte d;
+
+        a=10;
+        a2=3;
+
+        a++;
+
+        int result=a/a2;
+
+        System.out.println(a);
+
+        int balance=120;
+
+        if(balance>100){
+            System.out.println("ok");
+        }
+
+        else{
+            System.out.println("no");
+        }
+
+
+        //logical variable
+        boolean e = true;
+
+        //char var
+
+
+    }
+
+}
