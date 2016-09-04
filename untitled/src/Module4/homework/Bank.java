d65ef97be3aac0ab12cad12f052f3e97c3a6b577
usonictw <usonictw@bigmir.net>
2016-09-04 19:23:42 +0300

    Merge branch 'master' of https://github.com/usonictw/JavaCoreCourse
    # Conflicts:
    #	untitled/src/Module3/homework/Task3/CollegeStudent.java
    #	untitled/src/Module3/homework/Task3/Course.java
    #	untitled/src/Module3/homework/Task3/SpecialStudent.java
    #	untitled/src/Module3/homework/Task3/Student.java

---
+++ untitled/src/Module4/homework/Bank.java
@@ -0,0 +1,84 @@
+package Module4.homework;
+
+import java.util.Currency;
+
+/**
+ * Created by PC on ${DATA}.
+ */
+public abstract class Bank {
+
+    // announce field
+    private long id;
+    private String bankCountry;
+    private Currency currency;
+    private int numberOfEmployee;
+    private double avrSalaryOfEmployee;
+    private long rating;
+    private long totalCapital;
+
+    //creating abstract methods
+    public abstract int getLimitOfWithdrawal();
+    public abstract int getLimitOfFunding();
+    public abstract int getMonthlyRate();
+    public abstract int getCommission();
+    public abstract double moneyPaidForSalary();
+
+    // create getters and setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public int getNumberOfEmployee() {
+        return numberOfEmployee;
+    }
+
+    public double getAvrSalaryOfEmployee() {
+        return avrSalaryOfEmployee;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public long getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setBankCountry(String bankCountry) {
+        this.bankCountry = bankCountry;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public void setNumberOfEmployee(int numberOfEmployee) {
+        this.numberOfEmployee = numberOfEmployee;
+    }
+
+    public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public void setTotalCapital(long totalCapital) {
+        this.totalCapital = totalCapital;
+    }
+}
