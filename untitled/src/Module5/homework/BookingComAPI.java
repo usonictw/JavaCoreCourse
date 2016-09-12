be05f95ba7424e4d5bed2f749897b24391566769
usonictw <0937354341us>
2016-09-12 18:26:16 +0300

    module 5 in progress

--- untitled/src/Module5/homework/BookingComAPI.java
+++ untitled/src/Module5/homework/BookingComAPI.java
@@ -14,24 +14,24 @@ public class BookingComAPI implements API {
         rooms[0] = new Room(0001, 180, 2, new Date(), "City Hotel", "Kiev");
         rooms[1] = new Room(0002, 120, 1, new Date(), "CityRent", "Kiev");
         rooms[2] = new Room(0003, 80, 1, new Date(), "Hotel2", "Kiev");
-        rooms[3] = new Room(0004, 280, 2, new Date(), "", "Kiev");
-        rooms[4] = new Room(0005, 380, 2, new Date(), "City Hotel", "Kiev");
+        rooms[3] = new Room(0004, 280, 2, new Date(), "RoomRent", "Kiev");
+        rooms[4] = new Room(0005, 380, 2, new Date(), "City12", "Kiev");
 
     }
 
     @Override
     public Room[] findRooms(int price, int person, String cityName, String hotel) {
 
+        Room[] resultFindRooms = new Room[5];
         for (int i=0; i<5; i++) {
-            if (rooms[i].getPrice() == price && rooms[i].getPersons() == person && rooms[i].getCityName() == cityName && rooms[i].getHotelName() == hotel) {
-
-                Room[]
-
+            if (rooms[i].equals(new Room(0001, price, person, new Date(), hotel, cityName))) {
+            resultFindRooms[i] = rooms[i];
+            }
+            else{
+                resultFindRooms[i] = null;
             }
         }
-
-
-        return new Room[0];
+        return resultFindRooms;
     }
 
 
--- untitled/src/Module5/homework/TripAdvisorAPI.java
+++ untitled/src/Module5/homework/TripAdvisorAPI.java
@@ -1,15 +1,37 @@
 package Module5.homework;
 
+import java.util.Date;
+
 /**
  * Created by user on 12.09.2016.
  */
 public class TripAdvisorAPI implements API {
 
-    Room[] rooms = new Room[5];
+    private Room[] rooms = new Room[5];
+
+    public TripAdvisorAPI() {
+
+        rooms[0] = new Room(0001, 180, 2, new Date(), "City Hotel", "Kiev");
+        rooms[1] = new Room(0002, 120, 1, new Date(), "CityRent", "Kiev");
+        rooms[2] = new Room(0003, 80, 1, new Date(), "Hotel2", "Kiev");
+        rooms[3] = new Room(0004, 280, 2, new Date(), "CityU", "Kiev");
+        rooms[4] = new Room(0005, 380, 2, new Date(), "City Hotel", "Kiev");
+    }
 
     @Override
     public Room[] findRooms(int price, int person, String cityName, String hotel) {
 
-        return new Room[0];
+        Room[] resultFindRooms = new Room[5];
+
+        for (int i = 0; i < 5; i++){
+            if (rooms[i].equals(new Room(0003, price, person, new Date(), hotel, cityName))){
+
+                resultFindRooms[i] = rooms[i];
+            }
+            else{
+                resultFindRooms[i] = null;
+            }
+        }
+        return resultFindRooms;
     }
 }
