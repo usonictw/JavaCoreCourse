5d47d8b992048b4448041085821f648bd0b29bfd
usonictw <0937354341us>
2016-10-24 17:37:48 +0300

    started Task 4

---
+++ untitled/src/Module10/homework/Task4/MyException.java
@@ -0,0 +1,18 @@
+package Module10.homework.Task4;
+
+/**
+ * Created by user on 24.10.2016.
+ */
+public class MyException {
+
+    public void f () {
+
+
+    }
+
+    public void g() throws Exception {
+
+        throw new Exception("new Exception");
+
+    }
+}
