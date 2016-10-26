159f691469dfc887df0d1f8df902f06f3fe6bb00
usonictw <0937354341us>
2016-10-26 17:22:31 +0300

    Task4 finished

--- untitled/src/Module10/homework/Task4/Test.java
+++ untitled/src/Module10/homework/Task4/Test.java
@@ -3,14 +3,14 @@ package Module10.homework.Task4;
 /**
  * Created by user on 25.10.2016.
  */
-public class main {
+public class Test {
 
     public static void main(String[] args) {
 
         MyException myException = new MyException();
 
         try {
-            myException.f(10, 4);
+            myException.f(10, 40);
         }
         catch (NullPointerException npe){
 
