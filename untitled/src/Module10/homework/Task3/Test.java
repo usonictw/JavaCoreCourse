f18662b93dac2d9aba39d93880f8ce2c1dabe15e
usonictw <0937354341us>
2016-10-27 10:36:37 +0300

    Task3 Module10 finished

--- untitled/src/Module10/homework/Task3/Test.java
+++ untitled/src/Module10/homework/Task3/Test.java
@@ -9,10 +9,10 @@ public class Test {
 
     public static void main(String[] args) {
 
-        User user = new User();
-        user = null;
+        User user = null;
 
         try {
+
             user.getName();
         }
         catch (NullPointerException npe){
