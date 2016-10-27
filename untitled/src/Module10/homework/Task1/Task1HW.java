66847c3e6d38693a96f8e34ace94f7bae0b6569a
usonictw <0937354341us>
2016-10-27 10:40:44 +0300

    Task1 Module10 finished

--- untitled/src/Module10/homework/Task1/Task1HW.java
+++ untitled/src/Module10/homework/Task1/Task1HW.java
@@ -3,7 +3,7 @@ package Module10.homework.Task1;
 /**
  * Created by PC on ${DATA}.
  *
- * Task1: Create a class with a main( ) that throws an object of class Exception inside a try block. Give the constructor for
+ * Task1: Create a class with a Test( ) that throws an object of class Exception inside a try block. Give the constructor for
  * Exception a String argument. Catch the exception inside a catch clause and print the String argument.
  * Add a finally clause and print a message to prove you were there.
  */
