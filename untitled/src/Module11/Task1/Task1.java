1dcd76b86e859493277d402efc3c9bfdb0fab010
usonictw <0937354341us>
2016-11-07 17:27:23 +0200

    in progress

--- untitled/src/Module11/Task1/Task1.java
+++ untitled/src/Module11/Task1/Task1.java
@@ -4,15 +4,14 @@ import java.io.*;
 
 /**
  * Created by PC on ${DATA}.
- *
+ * <p>
  * You should create method which replace words in the File and returns String with replaced values
- a) read file, save to string var
- b) replace words
- c) return new string
- String replacer(Map<String, String> map)
-
+ * a) read file, save to string var
+ * b) replace words
+ * c) return new string
+ * String replacer(Map<String, String> map)
  */
-public class Task1{
+public class Task1 {
 
     private String everything2;
 
@@ -20,7 +19,7 @@ public class Task1{
 
 
         File file = new File(filePath);
-        if(file.createNewFile()){
+        if (file.createNewFile()) {
             System.out.println(filePath + " File created");
 
         } else {
@@ -30,40 +29,49 @@ public class Task1{
 
     }
 
-    public void writeToFile(String filePath) throws Exception{
+    public void writeToFile(String filePath) throws Exception {
 
-        String inputText = null;
+        String input = null;
+        StringBuilder inputText = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileWriter fileWriter = new FileWriter(filePath);
+        BufferedWriter bw = new BufferedWriter(fileWriter);
 
         int counter = 1;
-        while (counter > 0)
-
-
-
-
         try {
-
-            FileWriter fileWriter = new FileWriter(filePath);
-            BufferedWriter bw = new BufferedWriter(fileWriter);
+            while (counter > 0) {
+                input = br.readLine();
+                counter--;
+            }
+            try {
+                bw.write(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bw.close();
+            br.close();
         }
-        catch (IOException e){
-            System.out.println(" file not found ");
-        }
+    }
+
+    public String readFile(String pathFile) throws IOException{
+
+        FileReader fileReader = new FileReader(pathFile);
+        BufferedReader br = new BufferedReader(fileReader);
 
 
-
+        return null;
     }
 
     public static void main(String[] args) throws Exception {
 
         Task1 task1 = new Task1();
 
-        task1.createFile("hello");
+        //task1.createFile("/Users/user/IdeaProjects/JavaCoreCourse/file.txt");
+        task1.writeToFile("/Users/user/IdeaProjects/JavaCoreCourse/file.txt");
     }
-
-
-
-
 
 
 }
