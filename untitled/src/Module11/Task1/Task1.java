e8e6838307f37a1d0d475169b501d8e6dae915e5
usonictw <0937354341us>
2016-11-09 17:44:26 +0200

    in progress

--- untitled/src/Module11/Task1/Task1.java
+++ untitled/src/Module11/Task1/Task1.java
@@ -3,6 +3,8 @@ package Module11.Task1;
 import Module11.FileUtils;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by PC on ${DATA}.
@@ -15,27 +17,51 @@ import java.io.*;
  */
 public class Task1 {
 
-    public String readFile(String pathFile) throws IOException{
-        String result = null;
-        try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
-            StringBuffer sb = new StringBuffer();
-            String readLine = br.readLine();
-            while (readLine != null) {
-                sb.append(readLine);
+    public String readFile() throws IOException{
+        String result;
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter file name");
+            String fileName = br.readLine();
+            BufferedReader br1 = new BufferedReader(new FileReader(fileName + ".txt"));
+            while ( (result = br1.readLine())!= null) {
+                sb.append(result);
                 sb.append(System.lineSeparator());
             }
-            result = sb.toString();
+            br1.close();
         } catch (IOException e) {
             System.out.println("file not found");
         }
-        return result;
+        result = sb.toString();
+
+        Map<String, String> mapWord = new HashMap<>();
+
+        String[] temp = result.split("\r\n");
+
+
+        for (int i = 0; i > temp.length; i++) {
+            if(mapWord.containsKey(temp[i])){
+                temp[i] = " ";
+            }
+
+            System.out.println();
+        }
+
+        return sb.toString();
+    }
+
+    public String replaceWord (Map<String, String> param){
+
+        return  null;
     }
     public static void main(String[] args) throws Exception {
 
         Task1 task1 = new Task1();
 
         //FileUtils.createFile();
-        FileUtils.writeToFile();
+        //FileUtils.writeToFile();
+
+        System.out.println(task1.readFile());
 
 
     }
