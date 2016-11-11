621fff948556830cf65c43199ca087020a180a59
usonictw <0937354341us>
2016-11-11 11:27:48 +0200

    task4 correction

--- untitled/src/Module11/Task4/Task4.java
+++ untitled/src/Module11/Task4/Task4.java
@@ -1,19 +1,8 @@
 package Module11.Task4;
 
 import Module11.FileUtils;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * Created by PC on ${DATA}.
- * Check if file contains particular word. Pring 0 if no. Print number n which equals number of times it is contained in the file
- * a) read file, save to string var
- * b) calculate how many time the word occurs
- * c) print result
- * int checkWord(String word)
- */
 public class Task4 {
 
     public int checkWord(String word) throws IOException {
